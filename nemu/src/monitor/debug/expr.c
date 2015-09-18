042f895f4538bd2b54976b0c67bf91d7976e3fd5
tracer-ics2015 <tracer@njuics.org>
2015-09-18 11:13:34 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:13:34 up 2 days, 12:22,  2 users,  load average: 0.00, 0.01, 0.05 3278b009351716ea7c2f2ba090919fac80af4c54

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -35,7 +35,7 @@ static struct rule {
 	{"==", EQ},						// equal
 	{"!=", NEQ},					// not equal
 	{"&&", AND},					// and
-	{"||", OR},						// or
+	{"\\|\\|", OR},						// or
 	{"!",  NOT}						// not
 
 };
