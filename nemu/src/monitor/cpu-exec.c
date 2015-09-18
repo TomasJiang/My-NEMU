88c87bead9a389971bf4270dddb9a7dfffc51e0f
tracer-ics2015 <tracer@njuics.org>
2015-09-18 18:12:45 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:12:45 up 2 days, 19:22,  2 users,  load average: 0.00, 0.01, 0.05 ef5c69c1d86bd20898645be5fdbc589b403c472b

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -85,12 +85,12 @@ void cpu_exec(volatile uint32_t n) {
 		{
 			bool success = false;
 			uint32_t result = expr(wp->str, &success);
-			Log("result = %x", result);
+			Log("result = %d", result);
 			Assert(success, "Invalid expression!");
-			Log("result = %x", result);
+			Log("result = %d", result);
 			if(result != wp->oldvalue)
 			{
-				Log("result = %x", result);
+				Log("result = %d", result);
 				wp->oldvalue = result;
 				nemu_state = STOP;
 				Log("nemu_state = STOP ? %s", nemu_state == STOP ? "YES" : "NO");
