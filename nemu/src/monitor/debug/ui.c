1c9e1cea1678866c310b248e15ba6b453b832bc8
tracer-ics2015 <tracer@njuics.org>
2015-10-05 18:41:10 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:41:10 up 2 days,  8:09,  2 users,  load average: 0.00, 0.01, 0.05 74fc6992b87978d17c65525241762b6b158710b

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -135,7 +135,7 @@ static struct {
 	{ "si", "Step NUM instructions", cmd_si },
 	{ "info", "r-List of all registers and their contents\n\t  w-Print status of all watchpoints", cmd_info },
 	{ "p", "Print value of expression EXP", cmd_p },
-	{ "x", "Print content of address EXP", cmd_x },
+	{ "x", "Examine memory", cmd_x },
 	{ "w", "Set a watchpoint for an expression", cmd_w },
 	{ "d", "Delete a specified watchpoint", cmd_d }
 };
