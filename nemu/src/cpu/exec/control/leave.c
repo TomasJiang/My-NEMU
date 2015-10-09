f36d735412ea2ae9f263760f546beae176cd2507
tracer-ics2015 <tracer@njuics.org>
2015-10-10 01:30:00 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:30:00 up  7:31,  2 users,  load average: 0.00, 0.01, 0.05 a40ad5e4e99da2392085680edea39e8a6c1a6738

--- nemu/src/cpu/exec/control/leave.c
+++ nemu/src/cpu/exec/control/leave.c
@@ -9,7 +9,7 @@ make_helper(leave) {
 	// DEST = (SS:ESP); (* copy a dword *)
 	// ESP = ESP + 4;
 	cpu.esp = cpu.ebp;
-	cpu.ebp = cpu.esp;
+	cpu.ebp = swaddr_read(cpu.esp, 4);
 	cpu.esp += 4;
 	print_asm("leave");
 	return 1;
--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -7,7 +7,7 @@ make_helper(ret) {
 	// DEST = (SS:ESP); (* copy a dword *)
 	// ESP = ESP + 4;
 	
-	cpu.eip = cpu.esp;
+	cpu.eip = swaddr_read(cpu.esp, 4);
 	cpu.esp += 4;
 	print_asm("ret");
 	return 1;
