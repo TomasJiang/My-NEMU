1705552e5254e3abe0b662f92f4522944d29b96f
tracer-ics2015 <tracer@njuics.org>
2015-10-10 01:18:01 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:18:01 up  7:19,  2 users,  load average: 0.08, 0.03, 0.05 f19ff350ffeb63e226e4f54813ee940133282cd2

--- nemu/src/cpu/exec/control/jbe.c
+++ nemu/src/cpu/exec/control/jbe.c
@@ -2,7 +2,7 @@
 
 make_helper(jbe_rel8) {
 	if(cpu.eflags.CF == 1 || cpu.eflags.ZF == 1)
-		cpu.eip += instr_fetch(cpu.eip + 1, 1);
+		cpu.eip += instr_fetch(cpu.eip + 1, 1) << 24 >> 24;
 	print_asm("jbe 0x%x", cpu.eip + 2);
 	return 2;
 }
