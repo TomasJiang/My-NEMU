d84c5f86671e07320b4790c78136fc76295014f2
tracer-ics2015 <tracer@njuics.org>
2015-10-10 04:52:19 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  04:52:19 up 10:53,  2 users,  load average: 0.00, 0.01, 0.05 ef1c61f6787c925c81f42df4972b147453f16c40

--- nemu/src/cpu/exec/arith/atlas.c
+++ nemu/src/cpu/exec/arith/atlas.c
@@ -2,19 +2,19 @@
 #include "atlas.h"
 
 void print_operand(Operand *op) {
-	Log("ADD PRINTOP:\n");
+	Log("ADD PRINTOP:");
 	switch(op->type) {
 		case OP_TYPE_REG:
-			Log("type: OP_TYPE_REG\n");
-			Log("reg = 0x%x\n", op->reg);
+			Log("type: OP_TYPE_REG");
+			Log("reg = 0x%x", op->reg);
 		case OP_TYPE_MEM:
-			Log("type: OP_TYPE_MEM\n");
-			Log("reg = 0x%x\n", op->addr);
+			Log("type: OP_TYPE_MEM");
+			Log("reg = 0x%x", op->addr);
 		case OP_TYPE_IMM:
-			Log("type: OP_TYPE_IMM\n");
-			Log("reg = 0x%x\n", op->imm);
+			Log("type: OP_TYPE_IMM");
+			Log("reg = 0x%x", op->imm);
 	}
 	Log("size = %u", op->size);
-	Log("val = 0x%x\n", op->val);
+	Log("val = 0x%x", op->val);
 	return;
 }
