10162dbf3d7becc15ecd5a0c3a1277c85b29bd50
tracer-ics2015 <tracer@njuics.org>
2015-10-10 00:23:27 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:23:27 up  6:24,  2 users,  load average: 0.00, 0.01, 0.05 8ac26bd4e3c29aaba49ce20a147feff778e7b17b

--- nemu/src/cpu/exec/control/call.c
+++ nemu/src/cpu/exec/control/call.c
@@ -9,8 +9,8 @@ make_helper(call_rel32) {
 	// EIP = EIP + rel32;
 	cpu.esp -= 4;
 	swaddr_write(cpu.esp, 4, eip);
-	Log("eip = 0x%08x", eip);
+	Log("eip = 0x%08x", cpu.eip);
 	eip = eip + instr_fetch(eip + 1, 4);
-	Log("eip = 0x%08x", eip);
+	Log("eip = 0x%08x", cpu.eip);
 	return 5;
 }
