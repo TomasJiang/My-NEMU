ad43e61dc01f15b50d931547e433f940a4104bdd
tracer-ics2015 <tracer@njuics.org>
2015-10-10 17:56:00 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:56:00 up 23:57,  2 users,  load average: 0.00, 0.01, 0.05 37afa5d96cf87324272672caac989722659f78b

--- nemu/src/cpu/exec/data-mov/movzx-template.h
+++ nemu/src/cpu/exec/data-mov/movzx-template.h
@@ -5,27 +5,30 @@
 void print_operand(Operand *op);
 
 static void do_execute() {
+	printf("reg: %x\n", REG(op_dest->reg));
 	printf("op_src:\n");
 	print_operand(op_src);
 	printf("op_dest:\n");
 	print_operand(op_dest);
 
 #if DATA_BYTE == 1
-	if(ops_decoded.is_data_size_16)
-		op_dest->size = 2;
-	else
-		op_dest->size = 4;
-#endif
-#if DATA_BYTE == 2
-	op_dest->size = 4; 
+	if(ops_decoded.is_data_size_16) {
+		REG(op_dest->reg) = REG(op_dest->reg) >> 16 << 16;
+		REG(op_dest->reg) += op_src->val;
+	} else
+		REG(op_dest->reg) = op_src->val;
 #endif
 
+#if DATA_BYTE == 2
+	REG(op_dest->reg) = op_src->val;
+#endif
+
+	printf("reg: %x\n", REG(op_dest->reg));
 	printf("op_src:\n");
 	print_operand(op_src);
 	printf("op_dest:\n");
 	print_operand(op_dest);
 
-	OPERAND_W(op_dest, op_src->val);
 	print_asm_template2();
 }
 
