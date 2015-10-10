72cece719fe139aa2738f28361975313b28b9822
tracer-ics2015 <tracer@njuics.org>
2015-10-10 15:21:59 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:21:59 up 21:23,  2 users,  load average: 0.02, 0.02, 0.05 6c55e8ffc5a1402acefa8d2aa997d9c7be8768e3

--- nemu/src/cpu/exec/control/je.c
+++ nemu/src/cpu/exec/control/je.c
@@ -1,8 +1,10 @@
 #include "cpu/exec/helper.h"
 
 make_helper(je_rel8) {
+	swaddr_t temp = cpu.eip + instr_fetch(cpu.eip + 1, 1);
 	if(cpu.eflags.ZF) {
-		cpu.eip += instr_fetch(cpu.eip + 1, 1);
+		cpu.eip = temp;
 	}
+	print_asm("je 0x%x", temp);
 	return 2;
 }
