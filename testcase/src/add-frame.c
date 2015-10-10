4c79cef8710413c99abacc871e4871c79286ec1c
tracer-ics2015 <tracer@njuics.org>
2015-10-10 14:27:05 +0800

    >  run 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:27:05 up 20:28,  2 users,  load average: 0.00, 0.01, 0.05 2c6d42f71d256b5e4fba11aef2150fd91d8077b1

--- testcase/src/add-frame.c
+++ testcase/src/add-frame.c
@@ -4,12 +4,12 @@ int add(int a, int b)
 {
 	if(!b)
 		return a;
-	return 1 + add(a, b - 1);
+	return 2 + add(a - 1, b - 1);
 }
 
 int main()
 {
-	int a = 4, b = 1000;
+	int a = 256, b = 1000;
 	add(a, b);
 
 	return 0;
