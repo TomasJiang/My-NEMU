1ff36dca049743a2dc6d4ed6043a31792e63ede8
tracer-ics2015 <tracer@njuics.org>
2015-10-13 10:12:15 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:12:15 up 57 min,  2 users,  load average: 0.00, 0.01, 0.03 f180fb83f702ae031c0cee80769ec45690075041

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -16,7 +16,7 @@ void load_elf_tables(int argc, char *argv[]) {
 	Assert(argc == 2, "run NEMU with format 'nemu [program]'");
 	exec_file = argv[1];
 
-	printf("\nDEBUG: %s\n", exec_file);
+	printf("DEBUG: %s\n", exec_file);
 	FILE *fp = fopen(exec_file, "rb");
 	Assert(fp, "Can not open '%s'", exec_file);
 
