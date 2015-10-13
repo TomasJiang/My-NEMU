8a8f715eda8833cdf30ba74f6b54aa0bd29027c1
tracer-ics2015 <tracer@njuics.org>
2015-10-13 09:37:17 +0800

    >  compile kernel 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:37:17 up 22 min,  2 users,  load average: 0.04, 0.04, 0.02 97570f3e553ab9d2699605ecbf7e88431d9916a

---
+++ nemu/src/cpu/exec/control/jle.c
@@ -0,0 +1,10 @@
+#include "cpu/exec/helper.h"
+
+make_helper(jle_rel8) {
+	swaddr_t temp = cpu.eip + instr_fetch(cpu.eip + 1, 1);
+	if(cpu.eflags.ZF && cpu.eflags.SF !- cpu.eflags.OF) {
+		cpu.eip = temp;
+	}
+	print_asm("jle 0x%x", temp + 2);
+	return 2;
+}
