73b831e5fa5c1dbbca7329f4aaa628de284ea15f
tracer-ics2015 <tracer@njuics.org>
2015-11-26 21:12:24 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:12:24 up 22:03,  2 users,  load average: 0.00, 0.01, 0.05 e3aaa8dae32808fea09dd4fcada5c65af582b6e7

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -6,9 +6,7 @@ make_helper(concat3(instr, _, SUFFIX)) {
     MEM_W(REG(R_EDI), MEM_R(REG(R_ESI)));
     int flag = cpu.eflags.DF;
     cpu.edi += (-2 * flag + 1) * DATA_BYTE;
-    Log("movs: cpu.edi = %u", cpu.edi);
     cpu.esi += (-2 * flag + 1) * DATA_BYTE;
-    Log("movs: cpu.esi = %u", cpu.esi);
     print_asm(str(instr));
     return 1;
 }
