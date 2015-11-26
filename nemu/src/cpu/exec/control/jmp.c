33cf8b281e1d56f3c94fad17a7db0d15b3d660b4
tracer-ics2015 <tracer@njuics.org>
2015-11-26 10:24:56 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:24:56 up 11:15,  2 users,  load average: 0.06, 0.03, 0.05 6cc5dd8d54347743ef1a9c6cc092e6ca5d007b2b

--- nemu/src/cpu/exec/control/jmp.c
+++ nemu/src/cpu/exec/control/jmp.c
@@ -1,7 +1,21 @@
 #include "cpu/exec/helper.h"
 
-make_helper(jmp_rel8) {
+make_helper(jmp_rel_b) {
 	cpu.eip += instr_fetch(cpu.eip + 1, 1);
 	print_asm("jmp 0x%x", cpu.eip + 2);
 	return 2;
 }
+
+make_helper(jmp_rel_w) {
+	cpu.eip += instr_fetch(cpu.eip + 1, 2);
+	print_asm("jmp 0x%x", cpu.eip + 2);
+	return 3;
+}
+
+make_helper(jmp_rel_l) {
+	cpu.eip += instr_fetch(cpu.eip + 1, 4);
+	print_asm("jmp 0x%x", cpu.eip + 2);
+	return 5;
+}
+
+make_helper_v(jmp_rel)
