924f9e754ee768aacc07514a926d2d75abb37de1
tracer-ics2015 <tracer@njuics.org>
2015-11-26 21:03:06 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:03:06 up 21:54,  2 users,  load average: 0.04, 0.03, 0.05 c9dce2181215cf2c39dfb19e9c8111f5800925f4

--- nemu/src/cpu/exec/control/ret.c
+++ nemu/src/cpu/exec/control/ret.c
@@ -2,13 +2,22 @@
 
 make_helper(ret) {
 	// EIP = Pop()
-	// 
+	//
 	// Pop:
 	// DEST = (SS:ESP); (* copy a dword *)
 	// ESP = ESP + 4;
-	
+
 	cpu.eip = swaddr_read(cpu.esp, 4);
 	cpu.esp += 4;
 	print_asm("ret");
 	return 1;
+}
+
+make_helper(ret_i) {
+    uint32_t imm = instr_fetch(eip + 1, 2);
+    Log("imm = %u", imm);
+	cpu.eip = swaddr_read(cpu.esp, 4);
+	cpu.esp += 4 + imm;
+	print_asm("ret $0x%u", imm);
+    return 3;
 }
