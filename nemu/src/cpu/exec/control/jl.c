6e2af39490032f4937d1bf228d00ca40687315e1
tracer-ics2015 <tracer@njuics.org>
2015-11-26 15:39:03 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:39:03 up 16:30,  2 users,  load average: 0.00, 0.01, 0.05 807a51f459f476a1d080b51e06a5504edbdb749

--- nemu/src/cpu/exec/control/jl.c
+++ nemu/src/cpu/exec/control/jl.c
@@ -13,3 +13,24 @@ make_helper(jl_rel_b) {
 	return 2;
 }
 
+make_helper(jl_rel_w) {
+    int32_t k = instr_fetch(cpu.eip + 1, 2);
+	swaddr_t temp = cpu.eip + (k << 16 >> 16);
+	if(cpu.eflags.SF != cpu.eflags.OF) {
+		cpu.eip = temp;
+	}
+	print_asm("jl 0x%x", temp + 4);
+	return 3;
+}
+
+make_helper(jl_rel_l) {
+    int32_t k = instr_fetch(cpu.eip + 1, 4);
+	swaddr_t temp = cpu.eip + k;
+	if(cpu.eflags.SF != cpu.eflags.OF) {
+		cpu.eip = temp;
+	}
+	print_asm("jl 0x%x", temp + 6);
+	return 5;
+}
+
+make_helper_v(jl_rel);
--- nemu/src/cpu/exec/control/jl.h
+++ nemu/src/cpu/exec/control/jl.h
@@ -2,5 +2,6 @@
 #define __Jl_H__
 
 make_helper(jl_rel_b);
+make_helper(jl_rel_v);
 
 #endif
