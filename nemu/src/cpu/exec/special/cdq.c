059bd41f4bc3a30e50ed57f77e301611925e3070
tracer-ics2015 <tracer@njuics.org>
2015-11-26 13:36:49 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:36:49 up 14:27,  2 users,  load average: 0.02, 0.04, 0.05 2ecfe3aeac2d534bf60d30bdd7b9d2fcd4df1b98

--- nemu/src/cpu/exec/special/cdq.c
+++ nemu/src/cpu/exec/special/cdq.c
@@ -1,8 +1,8 @@
 #include "cpu/exec/helper.h"
 
 make_helper(cdq) {
-    if(cpu.eax < 0)
-        cpu.eax = 0x0FFFFFFF;
+    if((int32_t)cpu.eax < 0)
+        cpu.edx = 0xffffffff;
     else
         cpu.edx = 0;
     return 1;
