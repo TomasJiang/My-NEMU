09e439bc1893f6480fbecb823564a049fcf3091e
tracer-ics2015 <tracer@njuics.org>
2015-11-26 18:05:00 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:05:00 up 18:56,  2 users,  load average: 0.00, 0.01, 0.05 afd1ce0b4cf3c234a12f5d6627847a5f27fed7db

--- nemu/src/cpu/exec/control/jne.c
+++ nemu/src/cpu/exec/control/jne.c
@@ -2,7 +2,7 @@
 
 make_helper(jne_rel_b) {
 	swaddr_t temp = cpu.eip + instr_fetch(cpu.eip + 1, 1);
-	if(cpu.eflags.ZF) {
+	if(!cpu.eflags.ZF) {
 		cpu.eip = temp;
 	}
 	print_asm("jne 0x%x", temp + 2);
