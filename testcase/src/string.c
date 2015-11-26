a5e3213338f4fa0fd2dfd460d02f9720d8baef5f
tracer-ics2015 <tracer@njuics.org>
2015-11-27 02:59:20 +0800

    >  run 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  02:59:20 up 1 day,  3:50,  2 users,  load average: 0.09, 0.62, 0.73 ccdbef41540893bf82c18ef258487aff5c79c188

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -22,9 +22,9 @@ int main() {
 
 	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 
+    set_bp();
 	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 
-    set_bp();
 	HIT_GOOD_TRAP;
 	return 0;
 }
