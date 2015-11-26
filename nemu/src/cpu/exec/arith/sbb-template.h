e4f785ca988a5cecf6c04ad7ef5d309f55bf0cac
tracer-ics2015 <tracer@njuics.org>
2015-11-26 21:55:10 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:55:10 up 22:46,  2 users,  load average: 0.00, 0.01, 0.05 6bd426fafba6af29c3af2101265adfe3b428252

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -3,9 +3,27 @@
 #define instr sbb
 
 static void do_execute() {
-    int32_t tmp = op_src->val, step = 32 - 8 * DATA_BYTE;
+    int32_t tmp = op_src->val, step = 32 - 8 * DATA_BYTE, result;
     tmp = (tmp << step >> step) + cpu.eflags.CF;
-    OPERAND_W(op_dest, op_dest->val - tmp);
+    result = op_dest->val - tmp;
+    OPERAND_W(op_dest, result);
+
+    unsigned sf = 0x1 & (tmp >> 31);
+    unsigned df = 0x1 & (op_dest->val >> 31);
+    unsigned rf = 0x1 & (result >> 31);
+
+    cpu.eflags.OF = (!df && sf && rf) || (df && !sf && !rf);
+    cpu.eflags.SF = rf;
+    cpu.eflags.ZF = (result == 0);
+    cpu.eflags.CF = (uint32_t)(op_dest->val) < (uint32_t)(op_src->val);
+
+    result = 0xff & result;
+    unsigned cnt;
+    for(cnt = 0; result; ++cnt)
+        result &= (result - 1);
+    cpu.eflags.PF = !(cnt % 2);
+
+    print_asm_template2();
 }
 
 make_instr_helper(rm2r)
