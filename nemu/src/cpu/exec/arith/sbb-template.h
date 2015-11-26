0be709a20169dcf1c421c2770642eb22fdcb0c11
tracer-ics2015 <tracer@njuics.org>
2015-11-26 21:47:21 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:47:21 up 22:38,  2 users,  load average: 0.01, 0.02, 0.05 780f6a1ef12c5d2afaca1eeda3dc1d3d70ed515

--- nemu/src/cpu/exec/arith/sbb-template.h
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -4,8 +4,8 @@
 
 static void do_execute() {
     int32_t tmp = op_src->val, step = 32 - 8 * DATA_BYTE;
-    tmp = tmp << step >> step;
-    OPERAND_W(op_dest, op_dest->val - (tmp + cpu.eflags.CF));
+    tmp = (tmp << step >> step) + cpu.eflags.CF;
+    OPERAND_W(op_dest, op_dest->val - tmp);
 }
 
 make_instr_helper(rm2r)
