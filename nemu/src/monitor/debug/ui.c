56a530ae3aa5b6576cc502e42f8f2d822705e05d
tracer-ics2015 <tracer@njuics.org>
2015-11-26 08:45:16 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  08:45:16 up  9:36,  2 users,  load average: 0.00, 0.02, 0.05 b1d2fcf12b04ff293504b2f417265c601d3bb550

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -107,8 +107,7 @@ static int cmd_x(char *args) {
     char exp[30];
 	bool success;
 	sscanf(args, "%u %s", &n, exp);
-    Log("Exp = %s", exp);
-	addr = expr(args, &success);
+	addr = expr(exp, &success);
 	Assert(success, "Invalid expression\n");
 
 	for(i = 0; i < n; ++i)
