ee994f15e98f92e19f869497804e85da90f1f9fd
tracer-ics2015 <tracer@njuics.org>
2015-11-27 07:54:09 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  07:54:09 up 1 day,  8:45,  2 users,  load average: 0.29, 0.08, 0.07 63f27c6eed8f7a895c9de1e84580eaf65e2dd323

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -5,38 +5,37 @@ FLOAT F_mul_F(FLOAT a, FLOAT b) {
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-	return (a / b) << 16;
+    return (a / b) << 16;
 }
 
 FLOAT f2F(float a) {
-	return a * 65536;
+    return (a * (1 << 16));
 }
 
 FLOAT Fabs(FLOAT a) {
-	return a ^ (0x1 << 31);
+    return a & (1 << 31);
 }
 
 FLOAT sqrt(FLOAT x) {
-	FLOAT dt, t = int2F(2);
+    FLOAT dt, t = int2F(2);
 
-	do {
-		dt = F_div_int((F_div_F(x, t) - t), 2);
-		t += dt;
-	} while(Fabs(dt) > f2F(1e-4));
+    do {
+        dt = F_div_int((F_div_F(x, t) - t), 2);
+        t += dt;
+    } while(Fabs(dt) > f2F(1e-4));
 
-	return t;
+    return t;
 }
 
 FLOAT pow(FLOAT x, FLOAT y) {
-	/* we only compute x^0.333 */
-	FLOAT t2, dt, t = int2F(2);
+    /* we only compute x^0.333 */
+    FLOAT t2, dt, t = int2F(2);
 
-	do {
-		t2 = F_mul_F(t, t);
-		dt = (F_div_F(x, t2) - t) / 3;
-		t += dt;
-	} while(Fabs(dt) > f2F(1e-4));
+    do {
+        t2 = F_mul_F(t, t);
+        dt = (F_div_F(x, t2) - t) / 3;
+        t += dt;
+    } while(Fabs(dt) > f2F(1e-4));
 
-	return t;
+    return t;
 }
-
