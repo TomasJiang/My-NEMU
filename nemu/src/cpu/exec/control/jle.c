15a6d7aa104ee94b7f254d7247be62db28b14eec
tracer-ics2015 <tracer@njuics.org>
2015-11-26 10:46:39 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:46:39 up 11:37,  2 users,  load average: 0.01, 0.04, 0.05 a469ebee804b561915eab3e76b6eea9c37eeba2b

--- nemu/src/cpu/exec/control/jle.c
+++ nemu/src/cpu/exec/control/jle.c
@@ -1,9 +1,9 @@
 #include "cpu/exec/helper.h"
 
-make_helper(jle_rel8) {
-    // if less or equal
-    // => ZF = 1 or SF != OF
+// if less or equal
+// => ZF = 1 or SF != OF
 
+make_helper(jle_rel_b) {
     int32_t k = instr_fetch(cpu.eip + 1, 1);
 	swaddr_t temp = cpu.eip + (k << 24 >> 24);
 	if(cpu.eflags.ZF || cpu.eflags.SF != cpu.eflags.OF) {
@@ -12,3 +12,25 @@ make_helper(jle_rel8) {
 	print_asm("jle 0x%x", temp + 2);
 	return 2;
 }
+
+make_helper(jle_rel_w) {
+    int32_t k = instr_fetch(cpu.eip + 1, 2);
+	swaddr_t temp = cpu.eip + (k << 16 >> 16);
+	if(cpu.eflags.ZF || cpu.eflags.SF != cpu.eflags.OF) {
+		cpu.eip = temp;
+	}
+	print_asm("jle 0x%x", temp + 3);
+	return 3;
+}
+
+make_helper(jle_rel_l) {
+    int32_t k = instr_fetch(cpu.eip + 1, 4);
+	swaddr_t temp = cpu.eip + k;
+	if(cpu.eflags.ZF || cpu.eflags.SF != cpu.eflags.OF) {
+		cpu.eip = temp;
+	}
+	print_asm("jle 0x%x", temp + 5);
+	return 5;
+}
+
+make_helper_v(jle_rel)
--- nemu/src/cpu/exec/control/jle.h
+++ nemu/src/cpu/exec/control/jle.h
@@ -1,6 +1,7 @@
 #ifndef __JLE_H__
 #define __JLE_H__
 
-make_helper(jle_rel8);
+make_helper(jle_rel_b);
+make_helper(jle_rel_v);
 
 #endif
