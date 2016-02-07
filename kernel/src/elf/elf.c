fe2f6324f80a7c3b19e7f54fed8b5822fe0b11b3
tracer-ics2015 <tracer@njuics.org>
2016-02-07 19:44:47 +0800

    >  compile kernel 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:44:47 up 2 days,  8:23,  2 users,  load average: 0.01, 0.03, 0.05 3a2585042ee393c7ded726843e19307743c0f78

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -33,24 +33,29 @@ uint32_t loader() {
 
 	/* TODO: fix the magic number with the correct one */
 	// const uint32_t elf_magic = 0xBadC0de;
-	const uint32_t elf_magic = 0x7f454c46;
+	const uint32_t elf_magic = 0x464C457F;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
 	/* Load each program segment */
 	panic("please implement me");
-	for(; true; ) {
+    int ind;
+    for(ind = 0; ind < elf->e_phnum; ind++) {
+        ph = (void *)elf->e_phoff + ind * elf->e_phentsize;
 		/* Scan the program header table, load each segment into memory */
 		if(ph->p_type == PT_LOAD) {
 
 			/* TODO: read the content of the segment from the ELF file
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
+            ramdisk_read((void *)ph->p_vaddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
 
 
 			/* TODO: zero the memory region
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
+
+            memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
 
 
 #ifdef IA32_PAGE
