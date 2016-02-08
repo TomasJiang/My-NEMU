4f841f480986b3ec1ad2d4272780b12256f26c90
tracer-ics2015 <tracer@njuics.org>
2016-02-08 11:13:24 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:13:24 up 2 days, 23:52,  2 users,  load average: 0.00, 0.01, 0.05 5f7bd5fc67bc40a8ff0b26f31271a14d1ee48ac

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -5,7 +5,7 @@
 static void do_execute() {
     cpu.eip = op_src->val-2;
     Log("0x%x", op_src->val);
-    print_asm("jmp 0x%x", cpu.eip+3+DATA_BYTE);
+    print_asm("jmp 0x%x", cpu.eip+2);
     //Log("eip = 0x%x", cpu.eip);
 }
 
