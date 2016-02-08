9ba49fe5462cfaa1b5baf5f599d7f6e4e26838d6
tracer-ics2015 <tracer@njuics.org>
2016-02-09 04:45:52 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  04:45:52 up 3 days, 17:25,  2 users,  load average: 0.25, 0.40, 0.22 535133a5dc516bd02a6ae4e3d5d41b23d57362

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,8 +3,8 @@
 #define instr movs
 
 make_helper(concat3(instr, _, SUFFIX)) {
-    Log("esi = 0x%x, edi = 0x%x", REG(R_ESI), REG(R_EDI));
-    Log("Mem($esi) = 0x%x", MEM_R(REG(R_ESI)));
+    //Log("esi = 0x%x, edi = 0x%x", REG(R_ESI), REG(R_EDI));
+    //Log("Mem($esi) = 0x%x", MEM_R(REG(R_ESI)));
     MEM_W(REG(R_EDI), MEM_R(REG(R_ESI)));
     int flag = cpu.eflags.DF;
     cpu.edi += (-2 * flag + 1) * DATA_BYTE;
--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -12,7 +12,7 @@ make_helper(rep) {
 	}
 	else {
 		while(cpu.ecx) {
-            Log("ecx == 0x%x", cpu.ecx);
+            //Log("ecx == 0x%x", cpu.ecx);
 			exec(eip + 1);
 			count ++;
 			cpu.ecx --;
