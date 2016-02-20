331b6200ee99de1eb4ad9110558d46a2da8a4de3
tracer-ics2015 <tracer@njuics.org>
2016-02-20 21:00:52 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:00:52 up 12:14,  2 users,  load average: 0.00, 0.02, 0.05 a327ab25d5c9792c62fdf0a2c0548f997e6859f6

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -7,7 +7,7 @@ void dram_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-    return cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+    return cache_read(addr, len);
 	// uint32_t res = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
     // Log("res = 1x%x", res);
     // return res;
