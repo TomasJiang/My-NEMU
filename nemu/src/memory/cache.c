121a337f0daa2a11cd98d731840d41b7fc8a2c4a
tracer-ics2015 <tracer@njuics.org>
2016-02-20 22:02:23 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:02:23 up 13:15,  2 users,  load average: 0.15, 0.09, 0.06 38165882f2edab8e5323e9ecba11d1252f2f5fb9

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -56,19 +56,19 @@ uint32_t cache_read(uint32_t addr, size_t len) {
     Assert(len == 1 || len == 2 || len == 4, "cache read not 1/2/4");
     Log("cache_read: addr = 0x%x, len = %d", addr, len);
     uint32_t tag     = addr >> (CB_WIDTH + CC_SET_WIDTH);
-    Log("tag = 0x%x", tag);
+    // Log("tag = 0x%x", tag);
     uint32_t set_num = (addr & CC_SET_MASK) >> CB_WIDTH;
-    Log("set_num = 0x%x", set_num);
+    // Log("set_num = 0x%x", set_num);
     uint32_t offset  = addr & CB_BLOCK_MASK;
-    Log("addr = 0x%x, CB_BLOCK_MASK = 0x%x", addr, CB_BLOCK_MASK);
-    Log("offset  = 0x%x", offset);
+    // Log("addr = 0x%x, CB_BLOCK_MASK = 0x%x", addr, CB_BLOCK_MASK);
+    // Log("offset  = 0x%x", offset);
     int i;
     for (i = 0; i < CC_ROW_SIZE; ++i) {
         if (cache[set_num][i].valid && cache[set_num][i].tag == tag) {
             Log("hit");
             // TODO
             uint32_t res = (*(uint32_t *)(cache[set_num][i].block + offset));
-            Log("res = 0x%02x", res & (~0u >> ((4 - len) << 3)));
+            // Log("res = 0x%02x", res & (~0u >> ((4 - len) << 3)));
             return res & (~0u >> ((4 - len) << 3));
         }
     }
@@ -76,11 +76,11 @@ uint32_t cache_read(uint32_t addr, size_t len) {
     // missed
     Log("missed");
     uint8_t buf[2 * CB_SIZE];
-    Log("dram_read_block");
+    // Log("dram_read_block");
     dram_read_block(addr & ~CB_BLOCK_MASK, buf);
-    Log("find_row_write");
+    // Log("find_row_write");
     find_row_write(buf, set_num, tag);
-    Log("find_row_write finished");
+    // Log("find_row_write finished");
 
     for(i = 0; i < CB_SIZE; ++i)
         printf("%02x ", buf[i]);
@@ -90,9 +90,9 @@ uint32_t cache_read(uint32_t addr, size_t len) {
     printf("\n");
 
     if (offset + len > CB_SIZE) {
-        Log("dram_read_block");
+        // Log("dram_read_block");
         dram_read_block((addr & ~CB_BLOCK_MASK) + CB_SIZE, buf + CB_SIZE);
-        Log("find_row_write");
+        // Log("find_row_write");
         find_row_write(buf + CB_SIZE, set_num, tag);
     }
 
