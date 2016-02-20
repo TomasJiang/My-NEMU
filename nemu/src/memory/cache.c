65b5da219db50a73deb18d94f2ecca524cf95ee7
tracer-ics2015 <tracer@njuics.org>
2016-02-20 15:27:48 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:27:48 up  6:41,  2 users,  load average: 0.00, 0.01, 0.05 87605371ac7ebcb265d9b9eff48abac9ad1cddb

---
+++ nemu/src/memory/cache.c
@@ -0,0 +1,18 @@
+#include "common.h"
+#include "cache.h"
+
+uint32_t dram_read(hwaddr_t, size_t);
+
+uint32_t cache_read(uint32_t addr, size_t len) {
+    Log("addr = 0x%x, len = %d", addr, len);
+    uint32_t tag = addr >> (CB_WIDTH + CC_SET_WIDTH);
+    uint32_t set_num = (addr & CC_SET_MASK) >> CB_WIDTH;
+    uint32_t offset = addr & CB_ADDR_MASK;
+    int i;
+    for (i = 0; i < CC_ROW_SIZE; ++i) {
+        if (cache[set_num][i].valid && cache[set_num][i].tag == tag) {
+            return cache[set_num][i].block[offset];
+        }
+    }
+    return dram_read(addr, len) & (~0u >> ((4 - len) << 3));
+}
---
+++ nemu/src/memory/cache.h
@@ -0,0 +1,31 @@
+#ifndef __CACHE_H__
+#define __CACHE_H__
+
+// cache parameters
+#define CC_WIDTH     16
+#define CC_SET_WIDTH 3
+#define CB_WIDTH     6
+
+#define CC_ROW_WIDTH (CC_WIDTH - CC_SET_WIDTH - CB_WIDTH)
+#define CC_SIZE      (1 << CC_WIDTH)
+#define CC_SET_SIZE  (1 << CC_SET_WIDTH)
+#define CC_ROW_SIZE  (1 << CC_ROW_WIDTH)
+#define CB_SIZE      (1 << CB_WIDTH)
+#define CC_SET_MASK  (((1 << CC_SET_WIDTH) - 1) << CB_WIDTH)
+#define CB_ADDR_MASK ((1 << CB_WIDTH) - 1)
+
+typedef struct cache_block {
+    bool     valid;
+    uint32_t tag;
+    uint8_t  block[CB_SIZE];
+
+    // int  (*read ) (struct cache_block *this, uint32_t addr);
+    // void (*write) (struct cache_block *this, uint32_t addr, uint32_t data);
+} cache_block;
+
+cache_block cache[CC_SET_SIZE][CC_ROW_SIZE];
+
+uint32_t cache_read(uint32_t addr, size_t len);
+
+
+#endif
