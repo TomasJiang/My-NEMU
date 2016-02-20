b6edcae4c458ac472d0147a767408f257fba3f9c
tracer-ics2015 <tracer@njuics.org>
2016-02-20 23:10:25 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:10:25 up 14:23,  2 users,  load average: 0.00, 0.01, 0.05 c2eaab40ab0202637616eb4e761f48dafaae705

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -55,6 +55,9 @@ void find_row_write(uint8_t *buf, uint32_t set_num, uint32_t tag) {
 uint32_t cache_read(uint32_t addr, size_t len) {
     Assert(len == 1 || len == 2 || len == 4, "cache read not 1/2/4");
     Log("cache_read: addr = 0x%x, len = %d", addr, len);
+
+    bool is_hit = false;
+    uint8_t  buf[2 * CB_SIZE];
     uint32_t tag     = addr >> (CB_WIDTH + CC_SET_WIDTH);
     // Log("tag = 0x%x", tag);
     uint32_t set_num = (addr & CC_SET_MASK) >> CB_WIDTH;
@@ -62,31 +65,39 @@ uint32_t cache_read(uint32_t addr, size_t len) {
     uint32_t offset  = addr & CB_BLOCK_MASK;
     // Log("addr = 0x%x, CB_BLOCK_MASK = 0x%x", addr, CB_BLOCK_MASK);
     // Log("offset  = 0x%x", offset);
+
+
     int i;
     for (i = 0; i < CC_ROW_SIZE; ++i) {
         if (cache[set_num][i].valid && cache[set_num][i].tag == tag) {
             Log("hit");
-            // TODO
-            uint32_t res = (*(uint32_t *)(cache[set_num][i].block + offset));
-            Log("res = 0x%02x", res & (~0u >> ((4 - len) << 3)));
-            return res & (~0u >> ((4 - len) << 3));
+            is_hit = true;
+            memcpy(buf, cache[set_num][i].block, CB_SIZE);
         }
     }
+    if (!is_hit) {
+        Log("missed");
+        dram_read_block(addr & ~CB_BLOCK_MASK, buf);
+        find_row_write(buf, set_num, tag);
+    }
 
-    // missed
-    Log("missed");
-    uint8_t buf[2 * CB_SIZE];
-    // Log("dram_read_block");
-    dram_read_block(addr & ~CB_BLOCK_MASK, buf);
-    // Log("find_row_write");
-    find_row_write(buf, set_num, tag);
-    // Log("find_row_write finished");
 
     if (offset + len > CB_SIZE) {
-        // Log("dram_read_block");
-        dram_read_block((addr & ~CB_BLOCK_MASK) + CB_SIZE, buf + CB_SIZE);
-        // Log("find_row_write");
-        find_row_write(buf + CB_SIZE, set_num, tag);
+        set_num = (set_num + 1) % CC_SET_SIZE;
+        is_hit = false;
+        for (i = 0; i < CC_ROW_SIZE; ++i) {
+            if (cache[set_num][i].valid && cache[set_num][i].tag == tag) {
+                Log("hit");
+                is_hit = true;
+                memcpy(buf + CB_SIZE, cache[set_num][i].block, CB_SIZE);
+            }
+        }
+
+        if (!is_hit) {
+            Log("missed");
+            dram_read_block((addr & ~CB_BLOCK_MASK) + CB_SIZE, buf + CB_SIZE);
+            find_row_write(buf + CB_SIZE, set_num, tag);
+        }
     }
 
     // uint32_t res = unalign_rw(buf + offset, 4) & (~0u >> ((4 - len) << 3));
