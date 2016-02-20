0e2554483bc74b1d5978fcdd37a558be386fec32
tracer-ics2015 <tracer@njuics.org>
2016-02-21 01:07:50 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:07:50 up 16:21,  2 users,  load average: 0.66, 0.37, 0.24 703d06047ba0866323ccae2ab60008e55e864c4

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -2,28 +2,28 @@
 #define __CACHE_H__
 
 // cache parameters
-#define CC_WIDTH      16
-#define CC_SET_WIDTH  3
-#define CC_BLOCK_WIDTH      6
+#define L1_CC_WIDTH      16
+#define L1_CC_SET_WIDTH  3
+#define L1_CC_BLOCK_WIDTH      6
 
-#define CC_ROW_WIDTH  (CC_WIDTH - CC_SET_WIDTH - CC_BLOCK_WIDTH)
-#define CC_SIZE       (1 << CC_WIDTH)
-#define CC_SET_SIZE   (1 << CC_SET_WIDTH)
-#define CC_ROW_SIZE   (1 << CC_ROW_WIDTH)
-#define CC_BLOCK_SIZE       (1 << CC_BLOCK_WIDTH)
-#define CC_SET_MASK   (((1 << CC_SET_WIDTH) - 1) << CC_BLOCK_WIDTH)
-#define CC_BLOCK_MASK  ((1 << CC_BLOCK_WIDTH) - 1)
+#define L1_CC_ROW_WIDTH  (L1_CC_WIDTH - L1_CC_SET_WIDTH - L1_CC_BLOCK_WIDTH)
+#define L1_CC_SIZE       (1 << L1_CC_WIDTH)
+#define L1_CC_SET_SIZE   (1 << L1_CC_SET_WIDTH)
+#define L1_CC_ROW_SIZE   (1 << L1_CC_ROW_WIDTH)
+#define L1_CC_BLOCK_SIZE       (1 << L1_CC_BLOCK_WIDTH)
+#define L1_CC_SET_MASK   (((1 << L1_CC_SET_WIDTH) - 1) << L1_CC_BLOCK_WIDTH)
+#define L1_CC_BLOCK_MASK  ((1 << L1_CC_BLOCK_WIDTH) - 1)
 
 typedef struct cache_block {
     bool     valid;
     uint32_t tag;
-    uint8_t  block[CC_BLOCK_SIZE];
+    uint8_t  block[L1_CC_BLOCK_SIZE];
 
     // int  (*read ) (struct cache_block *this, uint32_t addr);
     // void (*write) (struct cache_block *this, uint32_t addr, uint32_t data);
 } cache_block;
 
-cache_block cache[CC_SET_SIZE][CC_ROW_SIZE];
+cache_block cache[L1_CC_SET_SIZE][L1_CC_ROW_SIZE];
 
 void init_cache();
 uint32_t cache_read(uint32_t addr, size_t len);
--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -5,15 +5,15 @@
 
 void init_cache() {
     // Log("init_cache");
-    // Log("CC_SIZE\t\t%u", CC_SIZE);
-    // Log("CC_SET_SIZE\t%u", CC_SET_SIZE);
-    // Log("CC_ROW_SIZE\t%u", CC_ROW_SIZE);
-    // Log("CC_BLOCK_SIZE\t\t%u", CB_SIZE);
-    // Log("CC_SET_MASK\t%x", CC_SET_MASK);
-    // Log("CC_BLOCK_MASK\t%x", CC_BLOCK_MASK);
+    // Log("L1_CC_SIZE\t\t%u", L1_CC_SIZE);
+    // Log("L1_CC_SET_SIZE\t%u", L1_CC_SET_SIZE);
+    // Log("L1_CC_ROW_SIZE\t%u", L1_CC_ROW_SIZE);
+    // Log("L1_CC_BLOCK_SIZE\t\t%u", CB_SIZE);
+    // Log("L1_CC_SET_MASK\t%x", L1_CC_SET_MASK);
+    // Log("L1_CC_BLOCK_MASK\t%x", L1_CC_BLOCK_MASK);
     int i, j;
-    for (i = 0; i < CC_SET_SIZE; ++i)
-        for (j = 0; j < CC_ROW_SIZE; ++j)
+    for (i = 0; i < L1_CC_SET_SIZE; ++i)
+        for (j = 0; j < L1_CC_ROW_SIZE; ++j)
             cache[i][j].valid = false;
 }
 
@@ -22,14 +22,14 @@ uint32_t dram_write(hwaddr_t, size_t, uint32_t);
 
 void dram_read_block(uint32_t addr, uint8_t *buf) {
     int i;
-    for (i = 0; i < CC_BLOCK_SIZE; ++i) {
-        buf[i] = dram_read((addr & ~CC_BLOCK_MASK) + i, 1);
+    for (i = 0; i < L1_CC_BLOCK_SIZE; ++i) {
+        buf[i] = dram_read((addr & ~L1_CC_BLOCK_MASK) + i, 1);
     }
 }
 
 uint32_t find_row(uint32_t set_num) {
     uint32_t i;
-    for (i = 0; i < CC_ROW_SIZE; ++i) {
+    for (i = 0; i < L1_CC_ROW_SIZE; ++i) {
         if (!cache[set_num][i].valid) {
             return i;
         }
@@ -37,12 +37,12 @@ uint32_t find_row(uint32_t set_num) {
 
     // choose one to replace
     // srand(time(0));
-    return rand() % CC_ROW_SIZE;
+    return rand() % L1_CC_ROW_SIZE;
 }
 
 void find_row_write(uint8_t *buf, uint32_t set_num, uint32_t tag) {
     uint32_t row_num = find_row(set_num);
-    memcpy(cache[set_num][row_num].block, buf, CC_BLOCK_SIZE);
+    memcpy(cache[set_num][row_num].block, buf, L1_CC_BLOCK_SIZE);
     cache[set_num][row_num].tag   = tag;
     cache[set_num][row_num].valid = true;
 }
@@ -50,23 +50,23 @@ void find_row_write(uint8_t *buf, uint32_t set_num, uint32_t tag) {
 void cache_read_prime(uint32_t addr, uint8_t *buf, uint32_t set_num, uint32_t tag) {
     bool is_hit = false;
     int i;
-    for (i = 0; i < CC_ROW_SIZE; ++i) {
+    for (i = 0; i < L1_CC_ROW_SIZE; ++i) {
         if (cache[set_num][i].valid && cache[set_num][i].tag == tag) {
             // Log("hit");
             is_hit = true;
-            memcpy(buf, cache[set_num][i].block, CC_BLOCK_SIZE);
+            memcpy(buf, cache[set_num][i].block, L1_CC_BLOCK_SIZE);
         }
     }
     if (!is_hit) {
         // Log("missed");
-        dram_read_block(addr & ~CC_BLOCK_MASK, buf);
+        dram_read_block(addr & ~L1_CC_BLOCK_MASK, buf);
         find_row_write(buf, set_num, tag);
     }
 }
 
 void print_buf(uint8_t *buf) {
     int i;
-    for (i = 0; i < CC_BLOCK_SIZE; ++i) {
+    for (i = 0; i < L1_CC_BLOCK_SIZE; ++i) {
         printf("%02x ", buf[i]);
     }
     printf("\n");
@@ -76,15 +76,15 @@ uint32_t cache_read(uint32_t addr, size_t len) {
     Assert(len == 1 || len == 2 || len == 4, "cache read not 1/2/4");
     // Log("cache_read: addr = 0x%x, len = %d", addr, len);
 
-    uint8_t  buf[2 * CC_BLOCK_SIZE];
-    uint32_t tag     = addr >> (CC_BLOCK_WIDTH + CC_SET_WIDTH);
-    uint32_t set_num = (addr & CC_SET_MASK) >> CC_BLOCK_WIDTH;
-    uint32_t offset  = addr & CC_BLOCK_MASK;
+    uint8_t  buf[2 * L1_CC_BLOCK_SIZE];
+    uint32_t tag     = addr >> (L1_CC_BLOCK_WIDTH + L1_CC_SET_WIDTH);
+    uint32_t set_num = (addr & L1_CC_SET_MASK) >> L1_CC_BLOCK_WIDTH;
+    uint32_t offset  = addr & L1_CC_BLOCK_MASK;
 
     cache_read_prime(addr, buf, set_num, tag);
-    if (offset + len > CC_BLOCK_SIZE) {
-        cache_read_prime(addr + CC_BLOCK_SIZE, // next block in memory
-                buf + CC_BLOCK_SIZE, (set_num + 1) % CC_SET_SIZE, tag);
+    if (offset + len > L1_CC_BLOCK_SIZE) {
+        cache_read_prime(addr + L1_CC_BLOCK_SIZE, // next block in memory
+                buf + L1_CC_BLOCK_SIZE, (set_num + 1) % L1_CC_SET_SIZE, tag);
     }
 
 
@@ -94,29 +94,29 @@ uint32_t cache_read(uint32_t addr, size_t len) {
 
 void cache_write_prime(uint32_t addr, uint8_t *buf, uint8_t *mask, uint32_t set_num, uint32_t tag) {
     int i;
-    for (i = 0; i < CC_ROW_SIZE; ++i) {
+    for (i = 0; i < L1_CC_ROW_SIZE; ++i) {
         if (cache[set_num][i].valid && cache[set_num][i].tag == tag) {
-            memcpy_with_mask(cache[set_num][i].block, buf, CC_BLOCK_SIZE, mask);
+            memcpy_with_mask(cache[set_num][i].block, buf, L1_CC_BLOCK_SIZE, mask);
         }
     }
 }
 
 void cache_write(uint32_t addr, size_t len, uint32_t data) {
     // Log("cache_write: addr = 0x%x, len = %d, data = 0x%x", addr, len, data);
-    uint32_t tag     = addr >> (CC_BLOCK_WIDTH + CC_SET_WIDTH);
-    uint32_t set_num = (addr & CC_SET_MASK) >> CC_BLOCK_WIDTH;
-    uint32_t offset  = addr & CC_BLOCK_MASK;
+    uint32_t tag     = addr >> (L1_CC_BLOCK_WIDTH + L1_CC_SET_WIDTH);
+    uint32_t set_num = (addr & L1_CC_SET_MASK) >> L1_CC_BLOCK_WIDTH;
+    uint32_t offset  = addr & L1_CC_BLOCK_MASK;
 
-    uint8_t  buf[2 * CC_BLOCK_SIZE];
-    uint8_t mask[2 * CC_BLOCK_SIZE];
-    memset(mask, 0, 2 * CC_BLOCK_SIZE);
+    uint8_t  buf[2 * L1_CC_BLOCK_SIZE];
+    uint8_t mask[2 * L1_CC_BLOCK_SIZE];
+    memset(mask, 0, 2 * L1_CC_BLOCK_SIZE);
     *(uint32_t *)(buf + offset) = data;
     memset(mask + offset, 1, len);
 
     cache_write_prime(addr, buf, mask, set_num, tag);
-    if (offset + len > CC_BLOCK_SIZE) {
-        cache_write_prime(addr + CC_BLOCK_SIZE,
-                buf + CC_BLOCK_SIZE, mask, (set_num + 1) % CC_SET_SIZE, tag);
+    if (offset + len > L1_CC_BLOCK_SIZE) {
+        cache_write_prime(addr + L1_CC_BLOCK_SIZE,
+                buf + L1_CC_BLOCK_SIZE, mask, (set_num + 1) % L1_CC_SET_SIZE, tag);
     }
 
     dram_write(addr, len, data);
