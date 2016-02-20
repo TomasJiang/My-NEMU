80876389cdd60afa843e8ad4e3af67bf340897bf
tracer-ics2015 <tracer@njuics.org>
2016-02-20 21:56:56 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:56:56 up 13:10,  2 users,  load average: 0.08, 0.05, 0.05 aeb23323b1be0fc694dfbb366e1440c2df4f6e42

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -96,7 +96,7 @@ uint32_t cache_read(uint32_t addr, size_t len) {
 
     // uint32_t res = unalign_rw(buf + offset, 4) & (~0u >> ((4 - len) << 3));
     uint32_t res = unalign_rw(buf + offset, 4);
-    Log("res = 0x%02x", res);
+    Log("res = 0x%x", res);
     return res & (~0u >> ((4 - len) << 3));
 }
 
