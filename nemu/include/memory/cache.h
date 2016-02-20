c62858e3058fa32d8320ba44bf8a15ecc612fabc
tracer-ics2015 <tracer@njuics.org>
2016-02-20 20:08:14 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:08:14 up 11:21,  2 users,  load average: 0.00, 0.04, 0.07 d3a2f24043885286d0076a7c4576eb6f4a560eb4

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -12,7 +12,7 @@
 #define CC_ROW_SIZE   (1 << CC_ROW_WIDTH)
 #define CB_SIZE       (1 << CB_WIDTH)
 #define CC_SET_MASK   (((1 << CC_SET_WIDTH) - 1) << CB_WIDTH)
-#define CB_BLOCK_MASK  ((1 << 6) - 1)
+#define CB_BLOCK_MASK  ((1 << CB_WIDTH) - 1)
 
 typedef struct cache_block {
     bool     valid;
