d92bafc21b607f95d7edb8cd64343601bbb6fdbb
tracer-ics2015 <tracer@njuics.org>
2016-02-21 00:53:14 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:53:14 up 16:06,  2 users,  load average: 0.00, 0.01, 0.05 84541106253c5ec2910098835b87015011e1f296

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -54,14 +54,14 @@ void cache_read_prime(uint32_t addr, uint8_t *buf, uint32_t set_num, uint32_t ta
     int i;
     for (i = 0; i < CC_ROW_SIZE; ++i) {
         if (cache[set_num][i].valid && cache[set_num][i].tag == tag) {
-            Log("hit");
+            // Log("hit");
             is_hit = true;
             memcpy(buf, cache[set_num][i].block, CB_SIZE);
         }
     }
     if (!is_hit) {
-        Log("missed");
-        Log("addr = 0x%x", addr);
+        // Log("missed");
+        // Log("addr = 0x%x", addr);
         dram_read_block(addr & ~CB_BLOCK_MASK, buf);
         find_row_write(buf, set_num, tag);
     }
@@ -77,7 +77,7 @@ void print_buf(uint8_t *buf) {
 
 uint32_t cache_read(uint32_t addr, size_t len) {
     Assert(len == 1 || len == 2 || len == 4, "cache read not 1/2/4");
-    Log("cache_read: addr = 0x%x, len = %d", addr, len);
+    // Log("cache_read: addr = 0x%x, len = %d", addr, len);
 
     uint8_t  buf[2 * CB_SIZE];
     uint32_t tag     = addr >> (CB_WIDTH + CC_SET_WIDTH);
@@ -88,14 +88,15 @@ uint32_t cache_read(uint32_t addr, size_t len) {
     print_buf(buf);
     print_buf(buf + CB_SIZE);
     if (offset + len > CB_SIZE) {
-        cache_read_prime(addr + CB_SIZE, buf + CB_SIZE, (set_num + 1) % CC_SET_SIZE, tag);
+        cache_read_prime(addr + CB_SIZE, // next block in memory
+                buf + CB_SIZE, (set_num + 1) % CC_SET_SIZE, tag);
         print_buf(buf);
         print_buf(buf + CB_SIZE);
     }
 
 
     uint32_t res = unalign_rw(buf + offset, 4);
-    Log("res = 0x%x", res & (~0u >> ((4 - len) << 3)));
+    // Log("res = 0x%x", res & (~0u >> ((4 - len) << 3)));
     return res & (~0u >> ((4 - len) << 3));
 }
 
