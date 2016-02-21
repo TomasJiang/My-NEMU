5b8a9c338d2c0b0d45bed944f1ba68bc1ebfefc5
tracer-ics2015 <tracer@njuics.org>
2016-02-21 22:29:20 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:29:20 up 1 day, 13:42,  2 users,  load average: 0.00, 0.01, 0.05 8bce95bf935d956fb52994e18bac924c8e9dacb6

--- nemu/src/cpu/exec/special/lgdt.c
+++ nemu/src/cpu/exec/special/lgdt.c
@@ -1,9 +1,7 @@
 #include "cpu/exec/helper.h"
 
 make_helper(lgdt) {
-    Log("eip = 0x%x", eip);
     uint32_t gdtdesc = instr_fetch(cpu.eip + 3, 4);
-    // Log("gdtdesc = 0x%x", gdtdesc);
     uint32_t limit = instr_fetch(gdtdesc, 2);
     uint32_t base  = instr_fetch(gdtdesc + 2, 4);
     cpu.gdtr.base  = base;
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -29,7 +29,6 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 
 uint32_t swaddr_read(swaddr_t addr, size_t len) {
 #ifdef DEBUG
-    Log("len = %d", len);
 	// assert(len == 1 || len == 2 || len == 4);
 #endif
 	return lnaddr_read(addr, len);
