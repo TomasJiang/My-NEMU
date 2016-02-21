33a96fda58029fa5a814ceb1eb8fad85e8dc53aa
tracer-ics2015 <tracer@njuics.org>
2016-02-21 22:03:18 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:03:18 up 1 day, 13:16,  2 users,  load average: 0.00, 0.01, 0.05 b9ff1cfb406d769bb920991215ed059cd4e452a6

--- nemu/src/cpu/exec/special/lgdt.c
+++ nemu/src/cpu/exec/special/lgdt.c
@@ -1,7 +1,8 @@
 #include "cpu/exec/helper.h"
 
 make_helper(lgdt) {
-    // uint16_t limit = instr_fetch(cpu.eip + 2, 2);
+    uint16_t gdtdesc = instr_fetch(cpu.eip + 4, 4);
+    Log("gdtdesc = 0x%x", gdtdesc);
     // uint32_t base  = instr_fetch(cpu.eip + 4, 4);
     // cpu.gdtr.base  = base;
     // cpu.gdtr.limit = limit;
