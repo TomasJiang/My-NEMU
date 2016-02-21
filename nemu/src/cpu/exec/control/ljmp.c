26f63dd565f00c9e4e7e22596ea7d76827a22225
tracer-ics2015 <tracer@njuics.org>
2016-02-22 04:18:33 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  04:18:33 up 1 day, 19:32,  2 users,  load average: 0.10, 0.05, 0.05 dc323cc55765d95640c1ee2c1a821fe77ac890d7

--- nemu/src/cpu/exec/control/ljmp.c
+++ nemu/src/cpu/exec/control/ljmp.c
@@ -4,7 +4,7 @@ make_helper(ljmp) {
     uint32_t lword = instr_fetch(eip + 1, 4);
     uint16_t word  = instr_fetch(eip + 5, 2);
     cpu.cs.selector  = word;
-    cpu.eip = lword;
+    cpu.eip = lword - 6;
 	print_asm("ljmp $0x%x, $0x%x", word, lword);
     return 6;
 }
