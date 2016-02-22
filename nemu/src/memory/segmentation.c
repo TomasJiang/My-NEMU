6ed7ad4a19af922f3261e4c92001de5589d7f8c5
tracer-ics2015 <tracer@njuics.org>
2016-02-22 10:07:26 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:07:26 up 2 days,  1:20,  2 users,  load average: 0.01, 0.02, 0.05 3b81c2a0faf4ecb77c790804775120b0b99c76f2

--- nemu/src/memory/segmentation.c
+++ nemu/src/memory/segmentation.c
@@ -18,10 +18,8 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len);
 
 lnaddr_t seg_translate(swaddr_t swaddr, size_t len, uint8_t sreg) {
     Log("swaddr = 0x%x", swaddr);
-    if (!cpu.cr0.PE) {
-        Log("res = 0x%02x", lnaddr_read(swaddr, len));
-        return lnaddr_read(swaddr, len);
-    }
+    if (!cpu.cr0.PE)
+        return swaddr;
     Log("segment");
     uint32_t segdesc_addr = cpu.gdtr.base + cpu.cs.index;
     // Log("addr = 0x%x", segdesc_addr);
