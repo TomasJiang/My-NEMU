cc1936c6c77b4c1a4cc616d6a1239fb0e0b0e555
tracer-ics2015 <tracer@njuics.org>
2016-02-23 04:43:17 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  04:43:17 up 2 days, 19:56,  2 users,  load average: 0.12, 0.06, 0.06 738101d4d3a8237eeb4f97075595ad3f91114c00

--- nemu/include/memory/page.h
+++ nemu/include/memory/page.h
@@ -1,20 +1,57 @@
 #ifndef __PAGE_H__
 #define __PAGE_H__
 
-typedef union {
+/* 32bit x86 uses 4KB page size */
+#define PAGE_SIZE					4096
+#define NR_PDE						1024
+#define NR_PTE						1024
+#define PAGE_MASK					(4096 - 1)
+#define PT_SIZE						((NR_PTE) * (PAGE_SIZE))
+
+
+/* the 32bit Page Directory(first level page table) data structure */
+typedef union PageDirectoryEntry {
+	struct {
+		uint32_t present             : 1;
+		uint32_t read_write          : 1;
+		uint32_t user_supervisor     : 1;
+		uint32_t page_write_through  : 1;
+		uint32_t page_cache_disable  : 1;
+		uint32_t accessed            : 1;
+		uint32_t pad0                : 6;
+		uint32_t page_frame          : 20;
+	};
+	uint32_t val;
+} PDE;
+
+/* the 32bit Page Table Entry(second level page table) data structure */
+typedef union PageTableEntry {
+	struct {
+		uint32_t present             : 1;
+		uint32_t read_write          : 1;
+		uint32_t user_supervisor     : 1;
+		uint32_t page_write_through  : 1;
+		uint32_t page_cache_disable  : 1;
+		uint32_t accessed            : 1;
+		uint32_t dirty               : 1;
+		uint32_t pad0                : 1;
+		uint32_t global              : 1;
+		uint32_t pad1                : 3;
+		uint32_t page_frame          : 20;
+	};
+	uint32_t val;
+} PTE;
+
+typedef PTE (*PT) [NR_PTE];
+
+typedef union PageTranslationAddress {
     struct {
-        uint32_t P               : 1;
-        uint32_t R_W             : 1;
-        uint32_t U_S             : 1;
-        uint32_t PAD0            : 2;
-        uint32_t A               : 1;
-        uint32_t D               : 1;
-        uint32_t PAD1            : 2;
-        uint32_t AVAIL           : 3;
-        uint32_t PAGE_FRAME_ADDR : 20;
+        uint32_t offset : 12;
+        uint32_t page   : 10;
+        uint32_t dir    : 10;
     };
     uint32_t val;
-} PageTableEntry;
+} PTA;
 
 hwaddr_t page_translate(lnaddr_t lnaddr);
 
