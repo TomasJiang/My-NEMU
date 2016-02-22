1661eb7023ce2395c893b3480fb98d1e1e6bc28d
tracer-ics2015 <tracer@njuics.org>
2016-02-23 05:46:21 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  05:46:21 up 2 days, 20:59,  2 users,  load average: 0.20, 0.07, 0.06 401fa03fbd72a2d6516e2dd91bfa9ff9a11e4c7e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,7 +1,9 @@
 #include "common.h"
 #include "memory/l1_cache.h"
-#include "memory/segmentation.h"
+//#include "memory/segmentation.h"
 //#include "memory/page.h"
+#include "cpu/reg.h"
+#include "x86-inc/mmu.h"
 
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
@@ -21,9 +23,24 @@ void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
 	// dram_write(addr, len, data);
 }
 
+hwaddr_t page_translate(lnaddr_t addr, size_t len) {
+    LinearAddress lnaddr;
+    lnaddr.val = addr;
+    Log("addr = 0x%x", addr);
+    Log("lnaddr.offset = 0x%x", lnaddr.offset);
+    Log("lnaddr.page   = 0x%x", lnaddr.page);
+    Log("lnaddr.dir    = 0x%x", lnaddr.dir);
+    PDE *pdir_entry_addr   = (PDE *)((cpu.cr3.page_directory_base << 12) + (lnaddr.dir << 2));
+    Assert(pdir_entry_addr->present, "Page directory entry's present bit is 0.");
+    PTE *ptable_entry_addr = (PTE *)((pdir_entry_addr->page_frame << 12) + (lnaddr.page << 2));
+    Assert(ptable_entry_addr->present, "Page table entry's present bit is 0.");
+    addr = (ptable_entry_addr->page_frame << 12) + lnaddr.offset;
+    return addr;
+}
+
 uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 #ifdef IA32_PAGE
-    if (cpu.cr0.PG)
+    if (cpu.cr0.PE && cpu.cr0.PG)
         addr = page_translate(addr);
 #endif
 	return hwaddr_read(addr, len);
@@ -31,7 +48,7 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 
 void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 #ifdef IA32_PAGE
-    if (cpu.cr0.PG)
+    if (cpu.cr0.PE && cpu.cr0.PG)
         addr = page_translate(addr);
 #endif
 	hwaddr_write(addr, len, data);
