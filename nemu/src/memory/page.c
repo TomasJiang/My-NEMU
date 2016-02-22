77049fc8598b54b201d2c974f2742246da78a9d7
tracer-ics2015 <tracer@njuics.org>
2016-02-23 05:09:25 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  05:09:25 up 2 days, 20:22,  2 users,  load average: 0.00, 0.01, 0.05 7371e3d98a6b53bb56c2d6654b5c651f4ad37548

--- nemu/src/memory/page.c
+++ nemu/src/memory/page.c
@@ -10,8 +10,8 @@ hwaddr_t page_translation(lnaddr_t addr) {
     Log("PTA.offset = 0x%x", page_translation_addr.offset);
     Log("PTA.page   = 0x%x", page_translation_addr.page);
     Log("PTA.dir    = 0x%x", page_translation_addr.dir);
-    PDE *pdir_entry_addr   = (PDE *)((cpu.cr3.PDBR << 12) + (page_translation_addr.dir << 2));
-    PTE *ptable_entry_addr = (PTE *)((pdir_entry_addr->page_frame << 12) + (page_translation_addr.page << 2));
-    addr = (ptable_entry_addr->page_frame << 12) + page_translation_addr.offset;
+    // PDE *pdir_entry_addr   = (PDE *)((cpu.cr3.PDBR << 12) + (page_translation_addr.dir << 2));
+    // PTE *ptable_entry_addr = (PTE *)((pdir_entry_addr->page_frame << 12) + (page_translation_addr.page << 2));
+    // addr = (ptable_entry_addr->page_frame << 12) + page_translation_addr.offset;
     return addr;
 }
