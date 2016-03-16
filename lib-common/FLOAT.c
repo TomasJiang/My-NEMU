a140c790aba1fa71254b744ef6a2bb3885ea1b79
tracer-ics2015 <tracer@njuics.org>
2016-03-16 21:19:20 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:19:20 up  2:01,  2 users,  load average: 0.16, 0.06, 0.06 87fd309c331874bee79b4dd3370c948024508f20

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -4,11 +4,11 @@ typedef unsigned int        uint32_t;
 typedef unsigned long long  uint64_t;
 
 FLOAT F_mul_F(FLOAT a, FLOAT b) {
-    return ((a >> 16) * (b >> 16)) << 16;
+    return ((uint64_t)a * (uint64_t)b) >> 16;
 }
 
 FLOAT F_div_F(FLOAT a, FLOAT b) {
-    return (a / b) << 16;
+    return (((uint64_t)a << 32) / (uint64_t)b) >> 16;
 }
 
 FLOAT f2F(float a) {
@@ -26,6 +26,11 @@ FLOAT f2F(float a) {
     return sign == 0 ? res : -res;
 }
 
+float F2f(FLOAT a) {
+    float f = (float)(a);
+    return f / (1 << 16);
+}
+
 FLOAT Fabs(FLOAT a) {
     uint32_t sign = (a >> 31) & 0x1;
     if (sign) {
@@ -36,16 +41,19 @@ FLOAT Fabs(FLOAT a) {
 }
 
 FLOAT sqrt(FLOAT x) {
+    // printf("\nsqrt(%f)\n", F2f(x));
     FLOAT dt, t = int2F(2);
 
     do {
+        // printf("x = %f, t = %f, F_div_F(x, t) = %f\n", F2f(x), F2f(t), F2f(F_div_F(x, t)));
         dt = F_div_int((F_div_F(x, t) - t), 2);
+        // printf("dt = %d, %f\n", dt, F2f(dt));
         t += dt;
+        // printf("t = %d, %f\n", t, F2f(t));
     } while(Fabs(dt) > f2F(1e-4));
 
     return t;
 }
-
 
 
 /* we only compute x^0.333 */
@@ -54,9 +62,59 @@ FLOAT pow(FLOAT x, FLOAT y) {
 
     do {
         t2 = F_mul_F(t, t);
+        // assert(F2f(t) * F2f(t) == F2f(t2));
+        // printf("x = %f, t2 = %f, F_div_F(x, t2) = %f\n", F2f(x), F2f(t2), F2f(F_div_F(x, t2)));
         dt = (F_div_F(x, t2) - t) / 3;
         t += dt;
     } while(Fabs(dt) > f2F(1e-4));
 
     return t;
 }
+
+
+/*
+int main() {
+    uint32_t a = f2F(0.5);
+    uint32_t b = f2F(0.5);
+    uint32_t c = f2F(-0.5);
+    // printf("f2F(0.5)  = 0x%x\n", a);
+    // printf("F2f(a)  = %f\n", F2f(a));
+    // printf("f2F(-0.5) = 0x%x\n", c);
+    // printf("F2f(c)  = %f\n", F2f(c));
+    // printf("Fabs(a) = 0x%x\n", Fabs(a));
+    // printf("Fabs(c) = 0x%x\n", Fabs(c));
+
+    FLOAT Fmul = F_mul_F(b, b);
+    // printf("F_mul_F(b, b) = 0x%x, %f\n", Fmul, F2f(Fmul));
+    FLOAT Fmuli = F_mul_int(a, 4);
+    // printf("F_mul_int(a, 4) = 0x%x, %f\n", Fmuli, F2f(Fmuli));
+    // printf("- F_mul_F(F_mul_int(a, 4), c) = 0x%x\n",
+    //         - F_mul_F(F_mul_int(a, 4), c));
+    FLOAT dt = F_mul_F(b, b) - F_mul_F(F_mul_int(a, 4), c);
+    // 0x14000 => 1.25
+    // printf("dt = 0x%x, %f\n", dt, F2f(dt));
+
+
+    // F_div_F, F_div_int
+
+
+    // printf("sqrt(16) = 0x%x, %f\n", sqrt(int2F(16)), F2f(sqrt(int2F(16))));
+    FLOAT sqrt_dt = sqrt(dt);
+    // printf("sqrt_dt = 0x%x, %f\n", sqrt_dt, F2f(sqrt_dt));
+
+    FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
+    printf("x1 = 0x%x, %f\n", x1, F2f(x1));
+    FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));
+    printf("x2 = 0x%x, %f\n", x2, F2f(x2));
+
+    // FLOAT x1_ans = f2F(0.618);
+    // printf("x1_ans = 0x%x\n", x1_ans);
+    // FLOAT x2_ans = f2F(-1.618);
+    // printf("x2_ans = 0x%x\n", x2_ans);
+
+	// printf("Fabs(x1_ans - x1) = 0x%x\n", Fabs(x1_ans - x1));
+	// printf("Fabs(x2_ans - x2) = 0x%x\n", Fabs(x2_ans - x2));
+
+    return 0;
+}
+*/
