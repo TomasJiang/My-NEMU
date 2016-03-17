b059e3f05f87ea662abb87acaf18b354842717c2
tracer-ics2015 <tracer@njuics.org>
2016-03-17 09:14:59 +0800

    >  compile kernel 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:14:59 up 13:57,  4 users,  load average: 0.01, 0.08, 0.16 ccb5fdf43ce2c8e91668effb5ef4cedf853714f1

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -17,7 +17,6 @@ void create_video_mapping();
 uint32_t get_ucr3();
 
 uint32_t loader() {
-	Log("kernal loader");
 	Elf32_Ehdr *elf;
 	Elf32_Phdr *ph = NULL;
 
@@ -32,53 +31,42 @@ uint32_t loader() {
 	elf = (void*)buf;
 
 	/* TODO: fix the magic number with the correct one */
-	// const uint32_t elf_magic = 0xBadC0de;
-	const uint32_t elf_magic = 0x464C457F;
+	const uint32_t elf_magic = 0x464c457f;
 	uint32_t *p_magic = (void *)buf;
 	nemu_assert(*p_magic == elf_magic);
 
-    // mm_malloc(0x08048000, 0x100);
+	
+
+/*#ifdef HAS_DEVICE
+	ide_read(ph_buf,elf->e_phoff,elf->e_phentsize*elf->e_phnum);
+#else
+	ramdisk_read(ph_buf,elf->e_phoff,elf->e_phentsize*elf->e_phnum);
+#endif */
 
 	/* Load each program segment */
-	//panic("please implement me");
-    int ind;
-    for(ind = 0; ind < elf->e_phnum; ind++) {
-        ph = (void *)elf->e_phoff + ind * elf->e_phentsize;
+	int i;
+	for(i=0 ; i < elf->e_phnum ; i++ ) 
+	{
 		/* Scan the program header table, load each segment into memory */
+		ph=(void *) elf->e_phoff + i * elf->e_phentsize;
 		if(ph->p_type == PT_LOAD) {
-
-			/* TODO: read the content of the segment from the ELF file
+			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-#ifndef HAS_DEVICE
-
-#ifdef IA32_PAGE
-            mm_malloc(ph->p_vaddr, ph->p_memsz);
-#endif
-            // p_vaddr == 0x800000
-
-            // TODO: p_filesz not 4 times
-            // p_filesz == 30
-            uint32_t trade_off = (ph->p_filesz + 3) / 4 * 4;
-            ramdisk_read((void *)ph->p_vaddr, ELF_OFFSET_IN_DISK + ph->p_offset, trade_off);
-            // ramdisk_read((void *)ph->p_vaddr, ELF_OFFSET_IN_DISK + ph->p_offset, ph->p_filesz);
-#else
-            panic("please implement me");
-#endif
-
-
-			/* TODO: zero the memory region
+			 //filesz = 30
+			 uint32_t trade_off = (ph->p_filesz + 3) / 4 * 4;
+			ramdisk_read((void*)ph->p_vaddr , ELF_OFFSET_IN_DISK + ph->p_offset , trade_off); 
+			 
+			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-
-            // memset((void *)ph->p_vaddr + ph->p_filesz, 0, ph->p_memsz - ph->p_filesz);
-
+			memset((void*)(ph->p_vaddr + ph->p_filesz) , 0 , ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */
 			extern uint32_t brk;
 			uint32_t new_brk = ph->p_vaddr + ph->p_memsz - 1;
-			if(brk < new_brk) { brk = new_brk; }
+     		if(brk < new_brk) { brk = new_brk; }
 #endif
 		}
 	}
