b2685788d91b7b0a091629fba972a17642ab86e5
tracer-ics2015 <tracer@njuics.org>
2016-04-30 06:57:16 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  06:57:16 up 10:19,  2 users,  load average: 0.04, 0.03, 0.06 62fd594eb85b644d9f22ccbe1c9a7412e8fffde9

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -177,7 +177,7 @@ helper_fun _2byte_opcode_table [256] = {
 /* 0x34 */	inv, inv, inv, inv,
 /* 0x38 */	inv, inv, inv, inv,
 /* 0x3c */	inv, inv, inv, inv,
-/* 0x40 */	inv, inv, inv, inv,
+/* 0x40 */	cmovo_r2rm_v, inv, inv, inv,
 /* 0x44 */	cmove_r2rm_v, inv, inv, inv,
 /* 0x48 */	inv, cmovs_r2rm_v, inv, inv,
 /* 0x4c */	inv, cmovge_r2rm_v, inv, inv,
