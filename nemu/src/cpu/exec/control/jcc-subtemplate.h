d4c0b360014efcb67dda7c769d7202f59f1566c6
tracer-ics2015 <tracer@njuics.org>
2016-04-30 15:28:17 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:28:17 up 18:50,  2 users,  load average: 0.00, 0.02, 0.05 4b937b2ccffaf63afb39f7b07210e2529e1b6c55

--- nemu/src/cpu/exec/control/jcc-subtemplate.h
+++ nemu/src/cpu/exec/control/jcc-subtemplate.h
@@ -4,7 +4,7 @@ make_helper(concat3(instr, _, SUFFIX)) {
     int32_t k = instr_fetch(cpu.eip + 1, DATA_BYTE);
     uint32_t step = 32 - (8 * DATA_BYTE);
     swaddr_t temp = cpu.eip + (k << step >> step);
-    Log("DATA_BYTE = %u, k = %u, step = %u, temp = %u", DATA_BYTE, k, step, temp);
+    Log("DATA_BYTE = %u, k = 0x%x, step = %u, temp = 0x%x", DATA_BYTE, k, step, temp);
     if(cond) {
         cpu.eip = temp;
     }
