27a1a1814c2566fc8223d7bd291a7c3b12cf01a2
tracer-ics2015 <tracer@njuics.org>
2016-04-30 17:58:12 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:58:12 up 21:20,  2 users,  load average: 0.00, 0.02, 0.05 5f49bb1569663ab4162f6e4be72eeab58ff38882

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,8 +3,8 @@
 #define instr movs
 
 make_helper(concat3(instr, _, SUFFIX)) {
-    Log("copy from 0x%x to 0x%x: %x", REG(R_ESI), REG(R_EDI), MEM_R(REG(R_ESI)));
-    MEM_W(REG(R_EDI), MEM_R(REG(R_ESI)));
+    Log("copy from 0x%x to 0x%x: %x", reg_l(R_ESI), reg_l(R_EDI), MEM_R(REG(R_ESI)));
+    MEM_W(reg_l(R_EDI), MEM_R(reg_l(R_ESI)));
     int flag = cpu.eflags.DF;
     cpu.edi += (-2 * flag + 1) * DATA_BYTE;
     cpu.esi += (-2 * flag + 1) * DATA_BYTE;
