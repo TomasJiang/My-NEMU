8dbf4128b14057bf471a53be41208c114e00c9e9
tracer-ics2015 <tracer@njuics.org>
2016-04-30 19:08:15 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:08:15 up 22:30,  2 users,  load average: 0.00, 0.06, 0.07 9622df57881e334327a775303dcd60b18de851df

--- nemu/src/cpu/exec/logic/shldi-template.h
+++ nemu/src/cpu/exec/logic/shldi-template.h
@@ -8,10 +8,10 @@ static void do_execute () {
 	DATA_TYPE out = op_src2->val;
 
 	uint8_t count = op_src->val;
-	count &= 0x1f;
+	count &= 0x3f;
 	while(count != 0) {
 		out <<= 1;
-		out |= (in & 1) << ((DATA_BYTE << 3) - 1);
+		out |= (in >> ((DATA_BYTE << 3) - 1)) & 0x1;
 		in <<= 1;
 		count --;
 	}
