6074bf8d8869212233d71ea55c60520b1b20d486
tracer-ics2015 <tracer@njuics.org>
2016-04-30 16:32:08 +0800

    >  compile NEMU 13307100105 atlas Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:32:08 up 19:54,  2 users,  load average: 0.03, 0.10, 0.07 5a090605afd7b29ed45c6a55545118f445503cd

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -9,22 +9,21 @@ static void do_execute() {
 	OPERAND_W(op_dest, result);
 
 	// OF, SF, ZF, CF, and PF
-    set_flags(op_src->val, op_dest->val, result, DATA_BYTE);
-    // uint32_t step = 0x8 * DATA_BYTE - 1;
-    // unsigned sf = 0x1 & (op_src->val >> step);
-    // unsigned df = 0x1 & (op_dest->val >> step);
-    // unsigned rf = 0x1 & (result >> step);
+    uint32_t step = 0x8 * DATA_BYTE - 1;
+    unsigned sf = 0x1 & (op_src->val >> step);
+    unsigned df = 0x1 & (op_dest->val >> step);
+    unsigned rf = 0x1 & (result >> step);
 
-    // cpu.eflags.OF = (!df && sf && rf) || (df && !sf && !rf);
-    // cpu.eflags.SF = rf;
-    // cpu.eflags.ZF = (result == 0);
-    // cpu.eflags.CF = (uint32_t)(op_dest->val) < (uint32_t)(op_src->val);
+    cpu.eflags.OF = (!df && sf && rf) || (df && !sf && !rf);
+    cpu.eflags.SF = rf;
+    cpu.eflags.ZF = (result == 0);
+    cpu.eflags.CF = (uint32_t)(op_dest->val) < (uint32_t)(op_src->val);
 
-	// result = 0xff & result;
-	// unsigned count;
-	// for(count = 0; result; ++count)
-	// 	result &= (result - 1);
-	// cpu.eflags.PF = !(count % 2);
+	result = 0xff & result;
+	unsigned count;
+	for(count = 0; result; ++count)
+		result &= (result - 1);
+	cpu.eflags.PF = !(count % 2);
 
 	print_asm_template2();
 }
